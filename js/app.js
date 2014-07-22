0f30921464bb84aa5b5b65281ed7245177bf92bd
n2diving <alexis@gormley-design.com>
2014-07-21 17:14:00 -0700

    working on switching background colors of divs based on which player clicked on which box

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ var gameApp = angular.module('TicTacToe', []);
 gameApp.controller('gameController', function ($scope) {
 
 $scope.sizeBox = 4;
-
+$scope.player1Turn = true;
+var x = $scope.x;
+var y = $scope.y;
 
   $scope.newBoard = function(size) {
     $scope.board = [];
@@ -14,8 +16,8 @@ $scope.sizeBox = 4;
         for (j=0; j<size; ++j ) {
           colset.push({
             x:(j + 1),
-            y:(i + 1)
-
+            y:(i + 1),
+            active:true
           });
         }
       $scope.board.push(colset);
@@ -26,13 +28,16 @@ $scope.sizeBox = 4;
   var player1moves = [];
   var player2moves = [];
 
+
   $scope.cluster = function(pos,player) {
     if($scope.player1Turn) {
       player1moves.push(pos);
-      $scope.board[pos] = "p1";
+      $scope.board[pos].active = false;
       console.log('Player 1');
       console.log(pos);
       console.log(player1moves);
+      console.log($scope.board);
+      console.log(player1moves[0][0]);
     }
     else {
       player2moves.push(pos);
@@ -40,6 +45,8 @@ $scope.sizeBox = 4;
       console.log('Player 2');
       console.log(pos);
       console.log(player2moves);
+      console.log($scope.board);
+
     }
     $scope.player1Turn = !$scope.player1Turn;
   }
