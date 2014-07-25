3e17acb2df09405b3d644142cc602b66bc0a7bd0
n2diving <alexis@gormley-design.com>
2014-07-24 18:56:33 -0700

    FINALLY...a working board...simple win logic, but working

--- js/app.js
+++ js/app.js
@@ -21,74 +21,98 @@ gameApp.controller('gameController', function ($scope, $firebase) {
   $scope.sizeBox = 3;
   $scope.player1Turn = true;
 
-  $scope.newBoard = function(size) {
-    // $scope.board = [];
-    $scope.board = [
-      {win: 1, status: 'active'},
-      {win: 2, status: 'active'},
-      {win: 4, status: 'active'},
-      {win: 8, status: 'active'},
-      {win: 16, status: 'active'},
-      {win: 32, status: 'active'},
-      {win: 64, status: 'active'},
-      {win: 128, status: 'active'},
-      {win: 256, status: 'active'},
-    ];
-};
+$scope.board = [[1,2,4],[8,16,32],[64,128,256]];
 
-  var player1moves = [];
-  var player2moves = [];
+//   $scope.newBoard = function(size) {
+//     $scope.board = [
+//       {win: 1, status: 'active'},
+//       {win: 2, status: 'active'},
+//       {win: 4, status: 'active'},
+//       {win: 8, status: 'active'},
+//       {win: 16, status: 'active'},
+//       {win: 32, status: 'active'},
+//       {win: 64, status: 'active'},
+//       {win: 128, status: 'active'},
+//       {win: 256, status: 'active'},
+//     ];
+// };
 
+  $scope.player1moves = 0;
+  $scope.player2moves = 0;
+  $scope.box_status = 'active';
+  // $scope.thisIsTheOne = function(box) {
+  //   $scope.player1moves += box;
+  //   console.log($scope.player1moves);
+  // }
 
-  $scope.cluster = function(pos,player) {
+  $scope.classSet = function(whatever, cell) {
+    return (whatever & cell) == cell;
+  }
+  
+
+  $scope.cluster = function(cell,player) {
     if($scope.player1Turn) {
-      player1moves.push(pos.win);
-      pos.status = 'p1';
+      $scope.player1moves = $scope.player1moves + cell;
+      $scope.box_status = 'p1';
       console.log('Player 1');
-      console.log(player1moves);
-      console.log(pos);
+      console.log($scope.player1moves);
+      $scope.win(cell);
     }
     else {
-      player2moves.push(pos.win);
-      pos.status = 'p2';
+      $scope.player2moves = $scope.player2moves + cell;
+      $scope.box_status = 'p2';
       console.log('Player 2');
-      console.log(player2moves);
+      console.log($scope.player2moves);
+      $scope.win(cell);
+
     }
     $scope.player1Turn = !$scope.player1Turn;
   };
 
 
-  $scope.win = function() {
     $scope.bWin = [  
       {check: 7, d: 'you win across the top!'},
       {check: 56, d: 'you win across the middle!'},
       {check: 448, d: 'you win across the bottom!'},
       {check: 73, d: 'you win down the left!'},
       {check: 146, d: 'you win down the middle!'},
-      {check: 282, d: 'you win down the right!'},
+      {check: 292, d: 'you win down the right!'},
       {check: 273, d: 'you win down diagonally!'},
       {check: 84, d: 'you win up diagonally!'},
       ];
 
+  $scope.win = function(box) {
       for(var bwIndex in $scope.bWin){
          var bw = $scope.bWin[bwIndex];
          if (($scope.player1moves & bw.check) == bw.check) {
            alert("Player 1, " + bw.d);
-           $scope.win();
+           $scope.p1winCounter++
+           console.log("p1 win count: " + $scope.p1winCounter);
+           $scope.reset();
          }
         };
 
       for(var bwIndex in $scope.bWin){
          var bw = $scope.bWin[bwIndex];
          if (($scope.player2moves & bw.check) == bw.check) {
-           alert("Player 2 has " + bw.d);
-           $scope.win();
+           alert("Player 2, " + bw.d);
+           $scope.p2winCounter++
+           console.log("p2 win count: " + $scope.p2winCounter);
+           $scope.reset();
          }
         };
+        
   };
 
+  $scope.p1winCounter = 0;
+  $scope.p2winCounter = 0;
 
-  $scope.newBoard($scope.sizeBox);
+  $scope.reset = function() {
+    $scope.player1moves = 0;
+    $scope.player2moves = 0;
+  }
+
+  // $scope.newBoard($scope.sizeBox);
 
 
 });
