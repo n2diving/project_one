b2a7150aaa5a2a268d0ac5074654dec2930dfe5f
n2diving <alexis@gormley-design.com>
2014-07-24 17:29:12 -0700

    simplified version with win logic

--- js/app.js
+++ js/app.js
@@ -2,97 +2,90 @@ var gameApp = angular.module('gameApp', ['firebase']);
 
 gameApp.controller('gameController', function ($scope, $firebase) {
 
-  var gameRef = new Firebase("https://glaring-fire-4974.firebaseIO.com");
+  // var gameRef = new Firebase("https://glaring-fire-4974.firebaseIO.com");
 
-  $scope.player1Turn = $firebase(new Firebase('https://glaring-fire-4974.firebaseIO.com' + '/player1Turn'));
-  $scope.remoteBoardContainer = $firebase(new Firebase('https://glaring-fire-4974.firebaseIO.com' + '/remoteBoardContainer'));
-  // $scope.remoteBoardContainer.$bind($scope, 'boardContainer: boardArray');
-  $scope.$watch('board', function() {
-    console.log('The times they are a changing!');
-  }) ;
+  // $scope.player1Turn = $firebase(new Firebase('https://glaring-fire-4974.firebaseIO.com' + '/player1Turn'));
+  // $scope.remoteBoardContainer = $firebase(new Firebase('https://glaring-fire-4974.firebaseIO.com' + '/remoteBoardContainer'));
+  // // $scope.remoteBoardContainer.$bind($scope, 'boardContainer: boardArray');
+  // $scope.$watch('board', function() {
+  //   console.log('The times they are a changing!');
+  // }) ;
 
-  $scope.boardContainer = {
-    boardArray: $scope.board,
-    playerTurn: $scope.player1Turn,
-    p1Moves: $scope.player1moves,
-    p2Moves: $scope.player2moves
-  }
+  // $scope.boardContainer = {
+  //   boardArray: $scope.board,
+  //   playerTurn: $scope.player1Turn,
+  //   p1Moves: $scope.player1moves,
+  //   p2Moves: $scope.player2moves
+  // }
 
   $scope.sizeBox = 3;
   $scope.player1Turn = true;
 
-  // var powerUp = function(size) {
-  //   for (n=0; n<4; ++n) {
-  //   return Math.pow(2, n)
-  //   console.log(powerUp);
-
-  //   }
-  // }
-
-
   $scope.newBoard = function(size) {
-    $scope.board = [];
+    // $scope.board = [];
+    $scope.board = [
+      {win: 1, status: 'active'},
+      {win: 2, status: 'active'},
+      {win: 4, status: 'active'},
+      {win: 8, status: 'active'},
+      {win: 16, status: 'active'},
+      {win: 32, status: 'active'},
+      {win: 64, status: 'active'},
+      {win: 128, status: 'active'},
+      {win: 256, status: 'active'},
+    ];
+};
 
-    for (i=0; i<size; ++i ) {
-      var colset = [];
-
-      for (j=0; j<size; ++j ) {
-        colset.push({
-          x:(j + 1),
-          y:(i + 1),
-          // win: powerUp($scope.sizeBox),
-          status: 'active'
-  });
-      }
-      $scope.board.push(colset);
-    } 
-  };
-
-  $scope.pc = 0;
   var player1moves = [];
   var player2moves = [];
 
 
   $scope.cluster = function(pos,player) {
-    var x = 0;
-    var y = 0;
     if($scope.player1Turn) {
-      player1moves.push(pos);
+      player1moves.push(pos.win);
       pos.status = 'p1';
       console.log('Player 1');
-      // console.log(pos);
       console.log(player1moves);
-      console.log('this is the pos keys' + pos.keys);
-      console.log('this is the scope index' + $scope.index);
-      // console.log($scope.board);
-      // console.log(player1moves[0][0]);
-      x++
+      console.log(pos);
     }
     else {
-      player2moves.push(pos);
+      player2moves.push(pos.win);
       pos.status = 'p2';
       console.log('Player 2');
-      // console.log(pos);
       console.log(player2moves);
-      console.log('this is the pos keys ' + pos.keys);
-      console.log('this is the scope index ' + $scope.index);
-      // console.log($scope.board);
-
     }
     $scope.player1Turn = !$scope.player1Turn;
   };
 
 
-    // $scope.win = function() {
-      // for(var boxIndex = $scope.box)
+  $scope.win = function() {
+    $scope.bWin = [  
+      {check: 7, d: 'you win across the top!'},
+      {check: 56, d: 'you win across the middle!'},
+      {check: 448, d: 'you win across the bottom!'},
+      {check: 73, d: 'you win down the left!'},
+      {check: 146, d: 'you win down the middle!'},
+      {check: 282, d: 'you win down the right!'},
+      {check: 273, d: 'you win down diagonally!'},
+      {check: 84, d: 'you win up diagonally!'},
+      ];
 
-    //   for(i=0, i < $scope.sizeBox, i++) {
-    //       
-    //    array[i].status == 'p1'
-    //   };
+      for(var bwIndex in $scope.bWin){
+         var bw = $scope.bWin[bwIndex];
+         if (($scope.player1moves & bw.check) == bw.check) {
+           alert("Player 1, " + bw.d);
+           $scope.win();
+         }
+        };
 
-    // };
-
+      for(var bwIndex in $scope.bWin){
+         var bw = $scope.bWin[bwIndex];
+         if (($scope.player2moves & bw.check) == bw.check) {
+           alert("Player 2 has " + bw.d);
+           $scope.win();
+         }
+        };
+  };
 
 
   $scope.newBoard($scope.sizeBox);
